cff2d60127ea6b5d5532b1ba88c2e68c63965c63
Gang Wang <gang.wang05@sap.com>
2021-08-26 14:25:27 +0800

    init a new project

---
+++ framework/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    this is home;
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+  }
+})
+</script>
