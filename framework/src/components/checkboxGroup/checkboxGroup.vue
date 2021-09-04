1082435309ac40825cd5d699c6f47775b0993e16
wanggang <arthurwanggang@yeah.net>
2021-09-04 23:58:30 +0800

    1: 增加checkboxGroup的基本结构

---
+++ framework/src/components/checkboxGroup/checkboxGroup.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="checkbox-group-container">
+
+  </div>
+
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "checkboxGroupCom",
+  setup () {
+
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
